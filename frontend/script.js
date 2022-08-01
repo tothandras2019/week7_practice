17f4935c404d0974f854a283994f6a95dc9746d1
Andras <toth.andras.2000@gmail.com>
2022-08-01 15:38:43 +0200

    sd

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,36 @@
 `use strict`;
+
+const scopeType = `Block scope, Function scope, Global scope`;
+
+let globalVariable = `global value`;
+
+//callback function: nem akkor fut le amikor megírjuk a függvényt, hanem akkor amikor meghívjuk.
+function logVariable() {
+  let functionVariable = `function value`;
+  let globalVariable = `inner global variable`;
+  // console.log(globalVariable);
+}
+
+// console.log(functionVariable);
+logVariable();
+// console.log(globalVariable);
+
+class App {
+  constructor() {
+    this.method();
+  }
+  method() {
+    logVariable();
+    function logVariable() {
+      const task = function () {
+        const inner = () => {
+          console.log(globalVariable);
+        };
+        inner();
+      };
+      task();
+    }
+  }
+}
+
+const app = new App();
